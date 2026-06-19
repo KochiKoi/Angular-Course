e562b6ff53a3f2fb640b2c09af87436e10a23258
BinSa0x <dinhkhoihoang1@gmail.com>
2026-06-19 22:24:14 +0700

    add folder part 3

---
+++ 01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, input, computed, Output, EventEmitter , output} from '@angular/core';
+import { DUMMY_USERS } from "./dummy-users"
+import { CardComponent } from "../shared/card/card.component";
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required:true}) user!:{
+    id : string;
+    avatar : string;
+    name : string;
+  };
+  @Output() select = new EventEmitter();
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.user.avatar;
+  })
+  onSelectUser() {
+this.select.emit(this.user.id);
+  }
+}
